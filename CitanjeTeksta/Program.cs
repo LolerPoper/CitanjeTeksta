75198ae2951dcef0ccfa76be494009480961ec40
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2022-12-07 09:24:35 +0100

    Add files via upload

---
+++ CitanjeTeksta/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+namespace CitanjeTeksta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\ucenik\Documents\LukaRadetic\datoteka.txt");
+            // Čitamo datoteku liniju po liniju
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            // Zatvaramo datoteku
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
